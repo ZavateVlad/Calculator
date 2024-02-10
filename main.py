ddf13494caae58db4b949f7294533076f3e8c4fc
ZavateVlad <95172779+ZavateVlad@users.noreply.github.com>
2024-02-10 12:25:19 +0000

    Added readable code

--- main.py
+++ main.py
@@ -14,30 +14,21 @@ def calculator(first_number, pick_op, second_number):
   return new_number
   
 first = float(input("What's your first number?: "))
+
 while new_calculation:
   symbols = ['+', '-', '*', '/']
+  
   for sign in symbols:
     print(sign)
+    
   op = input("Pick operation: ")
   second = float(input("What's your next number?: "))
   last_number = calculator(first, op, second)
   print(f"{first} {op} {second} = {last_number}")
   new = input(f"Type 'y' to continue calculating with {last_number}, or type 'n' to start new calculation: ").lower()
+  
   if new == 'y':
     first = last_number
   elif new =='n':
     first = int(input("What's your first number?: "))
     calculator(first, op, second)
-new_calculation = False
-    
-
-  
-
-
-
-  
-  
-  
-  
-
-
